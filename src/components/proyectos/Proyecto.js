ac5a6a1d5d53dd0329180e3db2679138116dcab3
Tirso del Álamo <tirsodelalamomartin@gmail.com>
2020-10-05 17:21:21 +0200

    Done

---
+++ src/components/proyectos/Proyecto.js
@@ -0,0 +1,30 @@
+import React, {useContext} from 'react';
+import proyectoContext from "../../context/proyectos/proyectoContext";
+import tareaContext from "../../context/tareas/tareaContext";
+
+const Proyecto = ({ proyecto }) => {
+    
+    //Obtener state de proyectos
+    const proyectosContext = useContext(proyectoContext);
+    const { proyectoActual } = proyectosContext;
+
+    //Obtener funcion del context de tarea
+    const tareasContext = useContext(tareaContext)
+    const {obtenerTareas} = tareasContext
+
+    //Funcion para agregar el proyecto actual
+    const seleccionarProyecto = id => {
+        proyectoActual(id) //Fijar proyecto actual
+        obtenerTareas(id) //Filtrar las tareas cuando hay evento click
+    }
+
+    return ( 
+        <li>
+            <button type='button' className='btn btn-blank' onClick = {() => seleccionarProyecto(proyecto._id)}>
+                {proyecto.nombre}
+            </button>
+        </li>
+     );
+}
+ 
+export default Proyecto;
